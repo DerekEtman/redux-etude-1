374e5a14a716ca6877968833e3d9d76a97592d3f
Derek Etman <dereketman@gmail.com>
2020-06-18 16:45:52 -0700

    Feat: Counter with Redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { createStore, bindActionCreators } from "redux";
+import { connect, Provider } from "react-redux";
+
+const initialState = {
+	count: 0,
+};
+
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
+
+const increment = () => ({
+	type: INCREMENT,
+});
+
+const decrement = () => ({
+	type: DECREMENT,
+});
+
+const resetValue = () => ({
+	type: RESET,
+});
+
+const reducer = (state = initialState, action) => {
+	if (action.type === INCREMENT) {
+		return {
+			count: state.count + 1,
+		};
+	} else if (action.type === DECREMENT) {
+		return {
+			count: state.count - 1,
+		};
+	}
+	return state;
+};
+
+const store = createStore(reducer);
+
+class Counter extends Component {
+	render() {
+		const { count, increment, decrement } = this.props;
+		console.log({ count, increment });
+
+		return (
+			<main className="Counter">
+				<p className="count">{count}</p>
+				<section className="Controls">
+					<button onClick={increment}>Increment</button>
+					<button onClick={decrement}>Decrement</button>
+					<button>Reset</button>
+				</section>
+			</main>
+		);
+	}
+}
+
+const mapStateToProps = (state) => {
+	return state;
+};
+
+const mapDispatchToProps ={
+  increment,
+  decrement,
+};
+
+const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+render(
+	<Provider store={store}>
+		<CounterContainer />
+	</Provider>,
+	document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
