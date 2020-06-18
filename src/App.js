95d740ac643534490fa2f18d113f6ea63492b8b8
Derek Etman <dereketman@gmail.com>
2020-06-18 14:11:59 -0700

    + Lesson 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+	compose,
+	createStore,
+	combineReducers,
+	bindActionCreators,
+	applyMiddleware,
+} from "redux";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const makeLouder = (string) => string.toUpperCase();
+	const repeatThreeTimes = (string) => string.repeat(3);
+	const embolden = (string) => string.bold();
+
+	const doIt = compose(embolden, repeatThreeTimes, makeLouder);
+  const store = createStore(reducer);
+	console.log(doIt("hello"));
+
+	const calcReducer = (state = { value: 1 }, action) => {
+		console.log("the action: ", action);
+
+		if (action.type === "ADD") {
+			const value = state.value;
+			const amount = action.payload.amount;
+
+			return {
+				value: value + amount,
+			};
+		}
+		return state;
+	};
+
+	const reducer = combineReducers({
+		calculator: calcReducer,
+	});
+
+	console.log(reducer);
+
+	const addAction = {
+		type: "ADD",
+		payload: {
+			amount: 4,
+		},
+	};
+
+	const createAddAction = (amount) => {
+		return {
+			type: "ADD",
+			payload: {
+				amount
+			},
+		};
+	};
+
+
+  const handrolledDispatched = store.dispatch(createAddAction(4));
+
+  const dispatchAdd = bindActionCreators(createAddAction, store.dispatch)
+	return <>hi</>;
 }
 
 export default App;
